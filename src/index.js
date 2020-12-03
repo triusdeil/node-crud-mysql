e82181c869591836b823736078c45df34712092a
triusdeil <triusdeil@gmail.com>
2020-12-03 14:30:21 -0400

    creando server con express

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 const express = require('express');
-const app = express()
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 5000);
+
+//server
+app.listen(app.get('port'), () =>{
+    console.log(`server on port ${app.get('port')}`);
+})
