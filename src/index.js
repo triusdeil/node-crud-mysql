b0a90cae040d3e3a91d3de17248236a452d6e5dd
triusdeil <triusdeil@gmail.com>
2020-12-03 14:46:57 -0400

    configurando el nodemon y el set de ejs y la direccion de la carpeta con path

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const path = require('path');
 
 //settings
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 4000);
+app.set('view engine', "ejs");
+app.set('views', path.join(__dirname,('views')));
 
 //middleware
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 //server
 app.listen(app.get('port'), () =>{
