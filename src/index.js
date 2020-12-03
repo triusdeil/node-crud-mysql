b53bcfac04bc81ac69f34074522e5ab81ceed53f
triusdeil <triusdeil@gmail.com>
2020-12-03 14:38:27 -0400

    instalando nodemon e inicializandolo y require morgan

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 //settings
 app.set('port', process.env.PORT || 5000);
+
+//middleware
+app.use(morgan('dev'))
 
 //server
 app.listen(app.get('port'), () =>{
